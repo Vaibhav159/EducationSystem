ffeeedb2c473878d82394997923da78d9d459494
Vaibhav159 <vlodha98@gmail.com>
2021-09-25 15:59:38 +0530

    removed django secret key

---
+++ EducationSystem/settings/local.py
@@ -0,0 +1,3 @@
+from EducationSystem.settings.settings import *
+
+SECRET_KEY = 'django-insecure-26&27^l%h5%zcbxlw0lvj3trzu!)vh)($=@os=pz8aj6@(r*rx'
--- EducationSystem/settings/settings.py
+++ EducationSystem/settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-26&27^l%h5%zcbxlw0lvj3trzu!)vh)($=@os=pz8aj6@(r*rx'
+SECRET_KEY = 'secure-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
